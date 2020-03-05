f34b6c1535791a00febff14866c3a069ac771958
Cliff Workman <cworkman@eborkdev.com>
2020-03-04 21:12:05 -0800

    bootstrapping project

---
+++ .profile
@@ -0,0 +1,2 @@
+export SCREEPS_TOKEN=ba80e8c4-220d-4be2-bb2e-0644afcaf333
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const loop = () => {
+    console.log(`Current game tick is ${Game.time}`);
+};
+
+module.exports = {
+    loop
+};

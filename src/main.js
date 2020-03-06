6c096ded0907ff781ba4a85784caa223108f10fc
Davic09 <davis.chris.gene@gmail.com>
2020-03-06 04:33:36 -0500

    Added the following: -Initial main module -Module for spawner logic to automatically spawn new harvester screeps when below 2 -Module for harvester logic to mine energy -Module to clean "dead" screeps from memory

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-"use strict"
+const roleHarvester = require('role.harvester');
+const spawner = require('spawner');
+const creepcleaner = require('creepcleaner');
 
 const loop = () => {
     console.log(`Current game tick is ${Game.time}`);
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+      }
+      spawner.run();
 };
 
 module.exports = {
---
+++ src/spawner.js
@@ -0,0 +1,24 @@
+const spawner = {
+  run: function () {
+    const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+        const newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'harvester'}});
+    }
+
+    if(Game.spawns['Spawn1'].spawning) {
+        const spawnCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawnCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            {align: 'left', opacity: 0.8});
+    }
+  }
+};
+
+module.exports = spawner;

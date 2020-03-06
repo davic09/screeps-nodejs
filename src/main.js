d781283dad52425065f2e918289e983409b202ba
Davic09 <davis.chris.gene@gmail.com>
2020-03-06 05:10:59 -0500

    Added upgrader and builder screep types as well as accompanying code to main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,13 @@ const loop = () => {
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-      }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
       spawner.run();
 };
 

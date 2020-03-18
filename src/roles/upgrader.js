84f0319e4add78ab4408161b10dbd18029fc68b8
Davic09 <davis.chris.gene@gmail.com>
2020-03-18 05:34:11 -0400

    Made updates to upgrader role that prevented the upgrader from harvesting as a last energy resort.

--- src/roles/upgrader.js
+++ src/roles/upgrader.js
@@ -2,6 +2,14 @@ const upgrader = {
     run: function(creep) {
       const energy = creep.store[RESOURCE_ENERGY]
       const mysource = creep.room.find(FIND_SOURCES) 
+      const deposits = creep.room
+          .find(FIND_STRUCTURES, {
+            filter: structure =>
+              ((structure.structureType == STRUCTURE_CONTAINER ||
+                structure.structureType == STRUCTURE_STORAGE) &&
+                structure.store[RESOURCE_ENERGY] > 0) 
+          });
+        deposits.sort();
       if (energy > 0) {
         if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
           creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: "#ffffff" } });
@@ -16,22 +24,14 @@ const upgrader = {
             creep.moveTo(ruin[0], { visualizePathStyle: { stroke: "#ffaa00" } });
           }
         }
-        if (energy == 0 && (ruin[0] === undefined || !ruin)) {
-        const deposits = creep.room
-          .find(FIND_STRUCTURES, {
-            filter: structure =>
-              ((structure.structureType == STRUCTURE_CONTAINER ||
-                structure.structureType == STRUCTURE_STORAGE) &&
-                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) 
-          });
-        deposits.sort();
+        if (energy == 0 && (ruin[0] === undefined || !ruin) && deposits[0] !== undefined) {
         if (creep.withdraw((deposits[0]), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(deposits[0], { visualizePathStyle: { stroke: "#ffaa00" } });
           }
         }
         else {
-          if (creep.harvest(mysource[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(mysource[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+          if (creep.harvest(mysource[1]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(mysource[1], { visualizePathStyle: { stroke: "#ffaa00" } });
           }
         }
       }

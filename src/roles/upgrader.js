f9284ee8814d256910d7e3e2a81ca348655a1d0a
Davic09 <davis.chris.gene@gmail.com>
2020-03-18 04:59:20 -0400

    Added work part to miner role.  This should be based on number of extensions in the future I think.  Allowed upgraders to farm sources if no other source of energy is available.

--- src/roles/upgrader.js
+++ src/roles/upgrader.js
@@ -1,6 +1,7 @@
 const upgrader = {
     run: function(creep) {
-      const energy = creep.store[RESOURCE_ENERGY] 
+      const energy = creep.store[RESOURCE_ENERGY]
+      const mysource = creep.room.find(FIND_SOURCES) 
       if (energy > 0) {
         if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
           creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: "#ffffff" } });
@@ -10,12 +11,12 @@ const upgrader = {
           filter: storage => 
           (storage.store[RESOURCE_ENERGY] > 0)
         });
-        if (creep.store.getFreeCapacity () > 0 && (ruin && ruin[0] !== undefined)) {
+        if (energy == 0 && (ruin && ruin[0] !== undefined)) {
           if (creep.withdraw(ruin[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(ruin[0], { visualizePathStyle: { stroke: "#ffaa00" } });
           }
         }
-        else {
+        if (energy == 0 && (ruin[0] === undefined || !ruin)) {
         const deposits = creep.room
           .find(FIND_STRUCTURES, {
             filter: structure =>
@@ -26,6 +27,11 @@ const upgrader = {
         deposits.sort();
         if (creep.withdraw((deposits[0]), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(deposits[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+          }
+        }
+        else {
+          if (creep.harvest(mysource[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(mysource[0], { visualizePathStyle: { stroke: "#ffaa00" } });
           }
         }
       }

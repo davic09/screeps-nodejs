7c664077492c020f9881da5a76ccc926eeb78dda
Davic09 <davis.chris.gene@gmail.com>
2020-03-19 00:23:25 -0400

    Fixed a bug where the conditions for getting energy and repairing were reversed.

--- src/roles/repairer.js
+++ src/roles/repairer.js
@@ -7,12 +7,12 @@ const repairer = {
     const energy = creep.store[RESOURCE_ENERGY]
     const droppedenergy = creep.room.find(FIND_DROPPED_RESOURCES);
           repairtargets.sort((a,b) => a.hits - b.hits);
-        if (energy > 0) {
+        if (energy == 0) {
             if (creep.pickup(droppedenergy[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(droppedenergy[0], { visualizePathStyle: { stroke: "#ffaa00" } });
             }
         }
-        if (energy == 0 ) {
+        if (energy > 0 ) {
             if (creep.repair(repairtargets[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(repairtargets[0], {visualizePathStyle: { stroke: "ffaa00"}});
             }
